47fa652a171ef7961c65792d2ecff69d7b591df5
heyy-kartik <kartikjagdale0511@gmail.com>
2025-11-23 16:53:43 +0530

    feat: add FeatureSteps component, remove CyberneticBentoGrid, and update image sources in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,22 +2,26 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'tailark.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "tailark.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'ik.imagekit.io',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'html.tailus.io',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "html.tailus.io",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
